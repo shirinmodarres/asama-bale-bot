a2d52ac4fe4df5cc5a67322dd0d903d9e94e2448
Shirin Modarres <shirinmodarres24@gmail.com>
2026-07-27 11:05:30 +0330

    fix: code store changed.

--- bot/services/user_service.py
+++ bot/services/user_service.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from bot.data.statuses import ACTIVE, PENDING_SELLER_APPROVAL, SELLER_REJECTED
 
 
-COMMISSION_PERCENT = 2
+COMMISSION_PERCENT = 4
 
 
 def calculate_commission(product_price: int) -> int:
